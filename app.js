e5de5e740c011f6b3e04340d5e3f91043b8f843e
Gabriel <Gabrielimperiano2000@gmail.com>
2024-04-08 14:58:47 -0300

    alterando o limite para 100

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 //paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
 
 let listaNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -18,7 +18,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
      exibirTextoNaTela('h1', 'Jogo do número secreto');
-     exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10');
+     exibirTextoNaTela('p', 'Escolha um numero entre 1 e 100');
 }
 
 exibirMensagemInicial();
